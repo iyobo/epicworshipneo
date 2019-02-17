ed63093e4d41cd215b20d6384e83f05da457280b
iyobo <iyobo@kindlycare.com>
2019-02-16 20:41:34 -0600

    checkpoint

--- app/data/AppStore.js
+++ app/data/AppStore.js
@@ -1,11 +1,16 @@
 import ScreenStore from "./ScreenStore";
 import ProductionStore from "./ProductionStore";
+import ElementStore from "./ElementStore";
+import { observable, action } from "mobx";
 
 class AppStore {
+
+  @observable busy = false;
 
   constructor(){
     this.screenStore = new ScreenStore(this);
     this.productionStore = new ProductionStore(this);
+    this.elementStore = new ElementStore(this);
   }
 
 
--- app/pages/ProductionPage.jsx
+++ app/pages/ProductionPage.jsx
@@ -5,9 +5,32 @@ import { inject, observer } from "mobx-react";
 @observer
 export default class ProductionPage extends Component {
   render() {
+    const activeProd = this.props.store.productionStore
+
     return (
       <div className='uk-animation-slide-right-small'>
-          <h2>Productions</h2>
+        <h2>Productions</h2>
+
+        <ul className="uk-iconnav">
+          <li><a href="#" uk-icon="icon: plus"></a></li>
+          <li><a href="#" uk-icon="icon: file-edit"></a></li>
+          <li><a href="#" uk-icon="icon: copy"></a></li>
+          <li><a href="#" uk-icon="icon: trash"></a></li>
+        </ul>
+
+        <div className="uk-grid-small uk-child-width-expand@s" data-uk-grid>
+          <div>
+            <ul className="uk-list uk-list-striped itemList">
+              <li>List item 2 <a href="#" uk-icon="icon: file-edit"></a> <a href="#" uk-icon="icon: trash"></a> </li>
+            </ul>
+          </div>
+          {}
+          <div className='uk-animation-slide-right-small'>
+            <div className="uk-card uk-card-default uk-card-body">Item</div>
+          </div>
+        </div>
+
+
       </div>
     );
   }
